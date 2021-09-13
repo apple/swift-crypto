8a3f64eae77cca247237e138005f7573e6945816
Adam Kopeć <adamkopec5@icloud.com>
2021-09-13 20:30:57 +0200

    Tidied up a bit both implementations of PBKDF2

--- Sources/Crypto/Key Derivation/BoringSSL/PBKDF2_boring.swift
+++ Sources/Crypto/Key Derivation/BoringSSL/PBKDF2_boring.swift
@@ -27,22 +27,21 @@ internal struct BoringSSLPBKDF2<H: HashFunction> {
     ///    - rounds: The number of rounds which should be used to perform key derivation.
     /// - Returns: The derived symmetric key.
     public static func deriveKey<Passphrase: DataProtocol, Salt: DataProtocol>(from password: Passphrase, salt: Salt, outputByteCount: Int, rounds: Int) throws -> SymmetricKey {
-        let digest: OpaquePointer
-        if H.self == Insecure.MD5.self {
-            digest = CCryptoBoringSSL_EVP_md5()
-        } else if H.self == Insecure.SHA1.self {
-            digest = CCryptoBoringSSL_EVP_sha1()
-        } else if H.self == SHA256.self {
-            digest = CCryptoBoringSSL_EVP_sha256()
-        } else if H.self == SHA384.self {
-            digest = CCryptoBoringSSL_EVP_sha384()
-        } else if H.self == SHA512.self {
-            digest = CCryptoBoringSSL_EVP_sha512()
-        } else {
-            // TODO: Use a better error
-            throw CryptoKitError.incorrectParameterSize
-        }
+        // MARK: This code should never be called, as every known `HashFunction` should also conform to `BoringSSLBackedHashFunction`
+        throw CryptoKitError.incorrectParameterSize
+    }
+}
 
+extension BoringSSLPBKDF2 where H: BoringSSLBackedHashFunction {
+    /// Derives a secure key using the provided hash function, passphrase and salt.
+    ///
+    /// - Parameters:
+    ///    - password: The passphrase, which should be used as a basis for the key. This can be any type that conforms to `DataProtocol`, like `Data` or an array of `UInt8` instances.
+    ///    - salt: The salt to use for key derivation.
+    ///    - outputByteCount: The length in bytes of resulting symmetric key.
+    ///    - rounds: The number of rounds which should be used to perform key derivation.
+    /// - Returns: The derived symmetric key.
+    public static func deriveKey<Passphrase: DataProtocol, Salt: DataProtocol>(from password: Passphrase, salt: Salt, outputByteCount: Int, rounds: Int) throws -> SymmetricKey {
         var derivedKeyData = SecureBytes(count: outputByteCount)
         let derivedCount = derivedKeyData.count
         let rc = derivedKeyData.withUnsafeMutableBytes { derivedKeyBytes -> Int32 in
@@ -55,9 +54,9 @@ internal struct BoringSSLPBKDF2<H: HashFunction> {
                 return passwordBytes.withUnsafeBytes { passwordBytes -> Int32 in
                     let passwordBuffer = passwordBytes.baseAddress!.assumingMemoryBound(to: Int8.self)
                     return CCryptoBoringSSL_PKCS5_PBKDF2_HMAC(passwordBuffer, password.count,
-                                                       saltBuffer, salt.count,
-                                                       UInt32(rounds), digest,
-                                                       derivedCount, keyBuffer)
+                                                              saltBuffer, salt.count,
+                                                              UInt32(rounds), H.digestType.dispatchTable,
+                                                              derivedCount, keyBuffer)
                 }
             }
         }
