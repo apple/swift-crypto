ccce9a2f34027aac7e251d3408812d11d590e9a3
Adam Kopeć <adamkopec5@icloud.com>
2021-09-13 23:05:35 +0200

    Rolled back previous changes and added test vectors

--- Sources/Crypto/Key Derivation/BoringSSL/PBKDF2_commoncrypto.swift
+++ Sources/Crypto/Key Derivation/BoringSSL/PBKDF2_commoncrypto.swift
@@ -26,41 +26,43 @@ internal struct CommonCryptoPBKDF2<H: HashFunction> {
     ///    - rounds: The number of rounds which should be used to perform key derivation.
     /// - Returns: The derived symmetric key.
     public static func deriveKey<Passphrase: DataProtocol, Salt: DataProtocol>(from password: Passphrase, salt: Salt, outputByteCount: Int, rounds: Int) throws -> SymmetricKey {
-        // MARK: This code should never be called, as every known `HashFunction` should also conform to `CommonCryptoHashFunction`
-        throw CryptoKitError.incorrectParameterSize
-    }
-}
-
-extension CommonCryptoPBKDF2 where H: CommonCryptoHashFunction {
-    /// Derives a secure key using the provided hash function, passphrase and salt.
-    ///
-    /// - Parameters:
-    ///    - password: The passphrase, which should be used as a basis for the key. This can be any type that conforms to `DataProtocol`, like `Data` or an array of `UInt8` instances.
-    ///    - salt: The salt to use for key derivation.
-    ///    - outputByteCount: The length in bytes of resulting symmetric key.
-    ///    - rounds: The number of rounds which should be used to perform key derivation.
-    /// - Returns: The derived symmetric key.
-    public static func deriveKey<Passphrase: DataProtocol, Salt: DataProtocol>(from password: Passphrase, salt: Salt, outputByteCount: Int, rounds: Int) throws -> SymmetricKey {
+        let ccHash: CCPBKDFAlgorithm
+        if H.self == Insecure.MD5.self {
+            ccHash = CCPBKDFAlgorithm(kCCHmacAlgMD5)
+        } else if H.self == Insecure.SHA1.self {
+            ccHash = CCPBKDFAlgorithm(kCCPRFHmacAlgSHA1)
+        } else if H.self == SHA256.self {
+            ccHash = CCPBKDFAlgorithm(kCCPRFHmacAlgSHA256)
+        } else if H.self == SHA384.self {
+            ccHash = CCPBKDFAlgorithm(kCCPRFHmacAlgSHA384)
+        } else if H.self == SHA512.self {
+            ccHash = CCPBKDFAlgorithm(kCCPRFHmacAlgSHA512)
+        } else {
+            // TODO: Use a better error
+            throw CryptoKitError.incorrectParameterSize
+        }
+        
         var derivedKeyData = Data(count: outputByteCount)
         let derivedCount = derivedKeyData.count
         let derivationStatus = derivedKeyData.withUnsafeMutableBytes { derivedKeyBytes -> Int32 in
             let keyBuffer: UnsafeMutablePointer<UInt8> =
-            derivedKeyBytes.baseAddress!.assumingMemoryBound(to: UInt8.self)
+                derivedKeyBytes.baseAddress!.assumingMemoryBound(to: UInt8.self)
             let saltBytes: ContiguousBytes = salt.regions.count == 1 ? salt.regions.first! : Array(salt)
             return saltBytes.withUnsafeBytes { saltBytes -> Int32 in
                 let saltBuffer = saltBytes.baseAddress!.assumingMemoryBound(to: UInt8.self)
                 let passwordBytes: ContiguousBytes = password.regions.count == 1 ? password.regions.first! : Array(password)
                 return passwordBytes.withUnsafeBytes { passwordBytes -> Int32 in
                     let passwordBuffer = passwordBytes.baseAddress!.assumingMemoryBound(to: Int8.self)
-                    return CCKeyDerivationPBKDF(CCPBKDFAlgorithm(kCCPBKDF2),
-                                                passwordBuffer,
-                                                password.count,
-                                                saltBuffer,
-                                                salt.count,
-                                                CCPBKDFAlgorithm(H.digestHmacType),
-                                                UInt32(rounds),
-                                                keyBuffer,
-                                                derivedCount)
+                    return CCKeyDerivationPBKDF(
+                        CCPBKDFAlgorithm(kCCPBKDF2),
+                        passwordBuffer,
+                        password.count,
+                        saltBuffer,
+                        salt.count,
+                        ccHash,
+                        UInt32(rounds),
+                        keyBuffer,
+                        derivedCount)
                 }
             }
         }
@@ -68,42 +70,6 @@ extension CommonCryptoPBKDF2 where H: CommonCryptoHashFunction {
             throw CryptoKitError.underlyingCoreCryptoError(error: derivationStatus)
         }
         return SymmetricKey(data: derivedKeyData)
-    }
-}
-
-// MARK: - CommonCryptoHashFunction
-
-protocol CommonCryptoHashFunction: HashFunction {
-    static var digestHmacType: Int { get }
-}
-
-extension Insecure.MD5: CommonCryptoHashFunction {
-    static var digestHmacType: Int {
-        kCCHmacAlgMD5
-    }
-}
-
-extension Insecure.SHA1: CommonCryptoHashFunction {
-    static var digestHmacType: Int {
-        kCCPRFHmacAlgSHA1
-    }
-}
-
-extension SHA256: CommonCryptoHashFunction {
-    static var digestHmacType: Int {
-        kCCPRFHmacAlgSHA256
-    }
-}
-
-extension SHA384: CommonCryptoHashFunction {
-    static var digestHmacType: Int {
-        kCCPRFHmacAlgSHA384
-    }
-}
-
-extension SHA512: CommonCryptoHashFunction {
-    static var digestHmacType: Int {
-        kCCPRFHmacAlgSHA512
     }
 }
 
---
+++ Tests/CryptoTests/Key Derivation/PBKDF2Tests.swift
@@ -0,0 +1,67 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCrypto open source project
+//
+// Copyright (c) 2021 Apple Inc. and the SwiftCrypto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.md for the list of SwiftCrypto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+import XCTest
+
+@testable import Crypto
+
+// Test Vectors are coming from https://tools.ietf.org/html/rfc6070
+class PBKDF2Tests: XCTestCase {
+    struct RFCTestVector: Codable {
+        var hash: String
+        var inputSecret: [UInt8]
+        var salt: [UInt8]
+        var rounds: Int
+        var outputLength: Int
+        var derivedKey: [UInt8]
+        
+        enum CodingKeys: String, CodingKey {
+            case hash = "Hash"
+            case inputSecret = "P"
+            case salt = "S"
+            case rounds = "c"
+            case outputLength = "dkLen"
+            case derivedKey = "DK"
+        }
+    }
+        
+    func oneshotTesting<H: HashFunction>(_ vector: RFCTestVector, hash: H.Type) throws {
+        let (contiguousSalt, discontiguousSalt) = vector.salt.asDataProtocols()
+        
+        let DK1 = try PBKDF2<H>.deriveKey(from: vector.inputSecret, salt: contiguousSalt,
+                                      outputByteCount: vector.outputLength,
+                                      rounds: vector.rounds)
+        
+        let DK2 = try PBKDF2<H>.deriveKey(from: vector.inputSecret, salt: discontiguousSalt,
+                                      outputByteCount: vector.outputLength,
+                                      rounds: vector.rounds)
+                
+        let expectedDK = SymmetricKey(data: vector.derivedKey)
+        XCTAssertEqual(DK1, expectedDK)
+        XCTAssertEqual(DK2, expectedDK)
+    }
+        
+    func testRFCVector<H: HashFunction>(_ vector: RFCTestVector, hash: H.Type) throws {
+        try oneshotTesting(vector, hash: hash)
+    }
+    
+    func testRfcTestVectorsSHA1() throws {
+        var decoder = try orFail { try RFCVectorDecoder(bundleType: self, fileName: "rfc-6070-PBKDF2-SHA1") }
+        let vectors = try orFail { try decoder.decode([RFCTestVector].self) }
+        
+        for vector in vectors {
+            precondition(vector.hash == "SHA-1")
+            try orFail { try self.testRFCVector(vector, hash: Insecure.SHA1.self) }
+        }
+    }
+}
