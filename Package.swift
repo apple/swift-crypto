be9943d02fa34a8d7333104534ebc108d297d820
Alex Shatberashvili <a_shatberashvili@apple.com>
2022-08-22 16:39:23 +0100

    Make BoringSSL wrapper match CryptoKit behaviour when working with x963 representation

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the SwiftCrypto open source project
@@ -56,7 +56,7 @@ if development {
 let package = Package(
     name: "swift-crypto",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v13),
         .iOS(.v13),
         .watchOS(.v6),
         .tvOS(.v13),
