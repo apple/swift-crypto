fa934db81ef2ad885a51d5eebfa50569a8dd03b5
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-12 10:08:40 -0700

    Switch to HexStrings for better readability.

--- Tests/_CryptoExtrasTests/ChaCha20CTRTests.swift
+++ Tests/_CryptoExtrasTests/ChaCha20CTRTests.swift
@@ -1,3 +1,4 @@
+
 //===----------------------------------------------------------------------===//
 //
 // This source file is part of the SwiftCrypto open source project
@@ -20,31 +21,31 @@ class ChaCha20CTRTests: XCTestCase {
 
     /// Test Vector - https://datatracker.ietf.org/doc/html/rfc9001#name-chacha20-poly1305-short-hea
     func testChaCha20CTR_v1() throws {
-        let hpKey: [UInt8] = [37, 162, 130, 185, 232, 47, 6, 242, 31, 72, 137, 23, 164, 252, 143, 27, 115, 87, 54, 133, 96, 133, 151, 208, 239, 203, 7, 107, 10, 183, 167, 164]
+        let hpKey = try Array(hexString: "25a282b9e82f06f21f488917a4fc8f1b73573685608597d0efcb076b0ab7a7a4")
         /// Sample = 0x5e5cd55c41f69080575d7999c25a5bfb
-        let counterAsData = Data([94, 92, 213, 92])
-        let counterAsUInt32: UInt32 = counterAsData.withUnsafeBytes { $0.load(as: UInt32.self) }
-        let iv: [UInt8] = [65, 246, 144, 128, 87, 93, 121, 153, 194, 90, 91, 251]
+        let counterAsData = try Array(hexString: "5e5cd55c")
+        let counterAsUInt32 = counterAsData.withUnsafeBytes { $0.load(as: UInt32.self) }
+        let iv = try Array(hexString: "41f69080575d7999c25a5bfb")
 
         let mask: Data = try Insecure.ChaCha20CTR.encrypt(Array<UInt8>(repeating: 0, count: 5), using: SymmetricKey(data: hpKey), counter: Insecure.ChaCha20CTR.Counter(data: counterAsData), nonce: Insecure.ChaCha20CTR.Nonce(data: iv))
         let mask2: Data = try Insecure.ChaCha20CTR.encrypt(Array<UInt8>(repeating: 0, count: 5), using: SymmetricKey(data: hpKey), counter: Insecure.ChaCha20CTR.Counter(offset: counterAsUInt32), nonce: Insecure.ChaCha20CTR.Nonce(data: iv))
 
-        XCTAssertEqual(mask, Data([174, 254, 254, 125, 3]))
+        XCTAssertEqual(mask, try Data(hexString: "aefefe7d03"))
         XCTAssertEqual(mask, mask2)
     }
 
     /// Test Vector - https://www.ietf.org/archive/id/draft-ietf-quic-v2-10.html#name-chacha20-poly1305-short-head
     func testChaCha20CTR_v2() throws {
-        let hpKey: [UInt8] = [214, 89, 118, 13, 43, 164, 52, 162, 38, 253, 55, 179, 92, 105, 226, 218, 130, 17, 209, 12, 79, 18, 83, 135, 135, 214, 86, 69, 213, 209, 184, 226]
+        let hpKey = try Array(hexString: "d659760d2ba434a226fd37b35c69e2da8211d10c4f12538787d65645d5d1b8e2")
         /// Sample = 0xe7b6b932bc27d786f4bc2bb20f2162ba
-        let counterAsData = Data([231, 182, 185, 50])
-        let counterAsUInt32: UInt32 = counterAsData.withUnsafeBytes { $0.load(as: UInt32.self) }
-        let iv: [UInt8] = [188, 39, 215, 134, 244, 188, 43, 178, 15, 33, 98, 186]
+        let counterAsData = try Array(hexString: "e7b6b932")
+        let counterAsUInt32 = counterAsData.withUnsafeBytes { $0.load(as: UInt32.self) }
+        let iv = try Array(hexString: "bc27d786f4bc2bb20f2162ba")
 
         let mask: Data = try Insecure.ChaCha20CTR.encrypt(Array<UInt8>(repeating: 0, count: 5), using: SymmetricKey(data: hpKey), counter: Insecure.ChaCha20CTR.Counter(data: counterAsData), nonce: Insecure.ChaCha20CTR.Nonce(data: iv))
         let mask2: Data = try Insecure.ChaCha20CTR.encrypt(Array<UInt8>(repeating: 0, count: 5), using: SymmetricKey(data: hpKey), counter: Insecure.ChaCha20CTR.Counter(offset: counterAsUInt32), nonce: Insecure.ChaCha20CTR.Nonce(data: iv))
 
-        XCTAssertEqual(mask, Data([151, 88, 14, 50, 191]))
+        XCTAssertEqual(mask, try Data(hexString: "97580e32bf"))
         XCTAssertEqual(mask, mask2)
     }
 
