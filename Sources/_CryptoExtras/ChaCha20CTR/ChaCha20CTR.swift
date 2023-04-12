bcb5992f5c4df794e0e40509f4b19800f497624a
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-12 09:33:05 -0700

    Corrected year in header

--- Sources/_CryptoExtras/ChaCha20CTR/ChaCha20CTR.swift
+++ Sources/_CryptoExtras/ChaCha20CTR/ChaCha20CTR.swift
@@ -2,7 +2,7 @@
 //
 // This source file is part of the SwiftCrypto open source project
 //
-// Copyright (c) 2022 Apple Inc. and the SwiftCrypto project authors
+// Copyright (c) 2023 Apple Inc. and the SwiftCrypto project authors
 // Licensed under Apache License v2.0
 //
 // See LICENSE.txt for license information
