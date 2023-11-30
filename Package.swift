d16dc2f9cd73127d1dff50b2e81ae6132202b883
Honza Dvorsky <honza@apple.com>
2023-11-30 10:23:46 +0100

    Add visionOS platform support (#38)
    ### Motivation
    
    While this isn't technically necessary, as all versions of a platform
    not explicitly mentioned are assumed to be supported, it's better to be
    explicit here.
    
    ### Modifications
    
    Add `visionOS(.v1)` to the list of supported platforms.
    
    ### Result
    
    Clearer support matrix.
    
    ### Test Plan
    
    N/A, this is basically just a documentation change.

--- Package.swift
+++ Package.swift
@@ -32,7 +32,7 @@ if ProcessInfo.processInfo.environment["SWIFT_OPENAPI_STRICT_CONCURRENCY"].flatM
 let package = Package(
     name: "swift-openapi-urlsession",
     platforms: [
-        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6),
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1)
     ],
     products: [
         .library(
