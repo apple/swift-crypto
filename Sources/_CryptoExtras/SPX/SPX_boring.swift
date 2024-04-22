5f3d032bd0c76e583ca752d2be2fbaca544a2ceb
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-04-22 17:01:51 +0200

    Update SPX implementation with more features and tests

--- Sources/_CryptoExtras/SPX/SPX_boring.swift
+++ Sources/_CryptoExtras/SPX/SPX_boring.swift
@@ -18,17 +18,62 @@ import Crypto
 @_implementationOnly import CryptoBoringWrapper
 import Foundation
 
-public struct BoringSSLSPXImpl {
-    public static func testSPX() -> Bool {
-        let publicKey = UnsafeMutablePointer<UInt8>.allocate(capacity: 32)
-        let secretKey = UnsafeMutablePointer<UInt8>.allocate(capacity: 64)
-        let message = [UInt8]("Hello, World!".utf8)
-        let messagePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: message.count)
-        messagePointer.initialize(from: message, count: message.count)
-        let signature = UnsafeMutablePointer<UInt8>.allocate(capacity: 7856)
+#if swift(>=5.8)
+@_documentation(visibility: public)
+public enum _SPX { }
+#else
+public enum _SPX { }
+#endif
+
+extension _SPX {
+    public struct PrivateKey: Sendable {
+        private let pointer: UnsafeMutablePointer<UInt8>
         
-        CCryptoBoringSSL_spx_generate_key(publicKey, secretKey)
-        CCryptoBoringSSL_spx_sign(signature, secretKey, messagePointer, message.count, 0)
-        return (CCryptoBoringSSL_spx_verify(signature, publicKey, messagePointer, message.count) == 1)
+        public init() {
+            self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: 64)
+            CCryptoBoringSSL_spx_generate_key(UnsafeMutablePointer<UInt8>.allocate(capacity: 32), self.pointer)
+        }
+        
+        public init(from seed: [UInt8]) {
+            self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: 64)
+            let seedPointer = UnsafeMutablePointer<UInt8>.allocate(capacity: 3 * 16)
+            seedPointer.initialize(from: seed, count: 3 * 16)
+            CCryptoBoringSSL_spx_generate_key_from_seed(UnsafeMutablePointer<UInt8>.allocate(capacity: 32), self.pointer, seedPointer)
+        }
+        
+        public var bytes: [UInt8] {
+            return Array(UnsafeBufferPointer(start: self.pointer, count: 64))
+        }
+        
+        public func signature(for message: [UInt8], randomized: Bool = false) -> [UInt8] {
+            let messagePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: message.count)
+            messagePointer.initialize(from: message, count: message.count)
+            let signature = UnsafeMutablePointer<UInt8>.allocate(capacity: 7856)
+            CCryptoBoringSSL_spx_sign(signature, self.pointer, messagePointer, message.count, randomized ? 1 : 0)
+            return Array(UnsafeBufferPointer(start: signature, count: 7856))
+        }
+    }
+}
+
+extension _SPX {
+    public struct PublicKey: Sendable {
+        private let pointer: UnsafeMutablePointer<UInt8>
+        
+        public init(privateKey: PrivateKey) {
+            self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: 32)
+            self.pointer.initialize(from: privateKey.bytes.suffix(32), count: 32)
+        }
+        
+        public var bytes: [UInt8] {
+            return Array(UnsafeBufferPointer(start: self.pointer, count: 32))
+        }
+        
+        public func isValidSignature(_ signature: [UInt8], for message: [UInt8]) -> Bool {
+            let messagePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: message.count)
+            messagePointer.initialize(from: message, count: message.count)
+            let signaturePointer = UnsafeMutablePointer<UInt8>.allocate(capacity: signature.count)
+            signaturePointer.initialize(from: signature, count: signature.count)
+            return (CCryptoBoringSSL_spx_verify(signaturePointer, self.pointer, messagePointer, message.count) == 1)
+        }
     }
 }
--- Tests/_CryptoExtrasTests/SPXTests.swift
+++ Tests/_CryptoExtrasTests/SPXTests.swift
@@ -19,6 +19,38 @@ import _CryptoExtras
 
 final class SPXTests: XCTestCase {
     func testSPX() {
-        XCTAssertTrue(BoringSSLSPXImpl.testSPX())
+        let privateKey = _SPX.PrivateKey()
+        let publicKey = _SPX.PublicKey(privateKey: privateKey)
+        let message = "Hello, World!".utf8.map { UInt8($0) }
+        let signature = privateKey.signature(for: message)
+        XCTAssertTrue(publicKey.isValidSignature(signature, for: message))
+    }
+
+    func testSPXWithSeed() {
+        // The seed provided here is 64 bytes long, but the SPX implementation only uses the first 48 bytes.
+        let seed: [UInt8] = (0..<64).map { _ in UInt8.random(in: 0...255) }
+        let privateKey = _SPX.PrivateKey(from: seed)
+        let publicKey = _SPX.PublicKey(privateKey: privateKey)
+        let message = "Hello, World!".utf8.map { UInt8($0) }
+        let signature = privateKey.signature(for: message)
+        XCTAssertTrue(publicKey.isValidSignature(signature, for: message))
+    }
+
+    func testSPXWithRandomizedSignature() {
+        let privateKey = _SPX.PrivateKey()
+        let publicKey = _SPX.PublicKey(privateKey: privateKey)
+        let message = "Hello, World!".utf8.map { UInt8($0) }
+        let signature = privateKey.signature(for: message, randomized: true)
+        XCTAssertTrue(publicKey.isValidSignature(signature, for: message))
+    }
+
+    func testSPXWithSeedAndRandomizedSignature() {
+        // The seed provided here is 64 bytes long, but the SPX implementation only uses the first 48 bytes.
+        let seed: [UInt8] = (0..<64).map { _ in UInt8.random(in: 0...255) }
+        let privateKey = _SPX.PrivateKey(from: seed)
+        let publicKey = _SPX.PublicKey(privateKey: privateKey)
+        let message = "Hello, World!".utf8.map { UInt8($0) }
+        let signature = privateKey.signature(for: message, randomized: true)
+        XCTAssertTrue(publicKey.isValidSignature(signature, for: message))
     }
 }
