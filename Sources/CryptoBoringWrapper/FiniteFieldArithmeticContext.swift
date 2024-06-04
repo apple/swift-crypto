e02700e07cfdfbdd6eab7335ccb04d0ece6a667e
Si Beaumont <beaumont@apple.com>
2024-06-04 10:46:50 +0100

    Add FiniteFieldArithmeticContext.inverse

--- Sources/CryptoBoringWrapper/FiniteFieldArithmeticContext.swift
+++ Sources/CryptoBoringWrapper/FiniteFieldArithmeticContext.swift
@@ -147,5 +147,23 @@ extension FiniteFieldArithmeticContext {
 
         return try ArbitraryPrecisionInteger(copying: actualOutputPointer)
     }
+
+    public func inverse(_ x: ArbitraryPrecisionInteger) throws -> ArbitraryPrecisionInteger {
+        var output = ArbitraryPrecisionInteger()
+
+        try x.withUnsafeBignumPointer { xPointer in
+            try self.fieldSize.withUnsafeBignumPointer { fieldSizePointer in
+                try output.withUnsafeMutableBignumPointer { outputPointer in
+                    let rc = CCryptoBoringSSL_BN_mod_inverse(outputPointer, xPointer, fieldSizePointer, self.bnCtx)
+                    guard rc == outputPointer else {
+                        throw CryptoBoringWrapperError.internalBoringSSLError()
+
+                    }
+                }
+            }
+        }
+
+        return output
+    }
 }
 #endif // CRYPTO_IN_SWIFTPM && !CRYPTO_IN_SWIFTPM_FORCE_BUILD_API
