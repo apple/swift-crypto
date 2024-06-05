bd1e490cd08700e69efc5cf2cc3385a8b902002f
Si Beaumont <beaumont@apple.com>
2024-06-05 19:47:02 +0100

    Fix test function names for Linux test discovery

--- Tests/_CryptoExtrasTests/TestRSABlindSigningAPI.swift
+++ Tests/_CryptoExtrasTests/TestRSABlindSigningAPI.swift
@@ -16,32 +16,6 @@ import Crypto
 import _CryptoExtras  // NOTE: No @testable import, because we want to test the public API.
 
 final class TestRSABlindSigningAPI: XCTestCase {
-    func testEndToEnd(parameters: _RSA.BlindSigning.Parameters<SHA384>, keySize: _RSA.Signing.KeySize) throws {
-        // [Issuer] Create key-pair (other initializers are available).
-        let privateKey = try _RSA.BlindSigning.PrivateKey(keySize: .bits2048, parameters: parameters)
-
-        // [Client] Create public key (other initializers are available).
-        let publicKey = privateKey.publicKey
-
-        // [Client] Have a message they wish to use.
-        let message = Data("This is some input data".utf8)
-
-        // [Client] Prepare the message.
-        let preparedMessage = publicKey.prepare(message)
-
-        // [Client] Blind the message to send to the server and get its blinding inverse.
-        let (blindedMessage, blindInverse) = try publicKey.blind(preparedMessage)
-
-        // [Issuer] Blind sign, construting the blinded message from the bytes received from the client.
-        let blindSignature = try privateKey.blindSignature(for: blindedMessage)
-
-        // [Client] Finalize using the blind inverse to unblind the signature.
-        let unblindedSignature = try publicKey.finalize(blindSignature, for: preparedMessage, blindInverse: blindInverse)
-
-        // [Verifier] Verify the unblinded signature.
-        XCTAssert(publicKey.isValidSignature(unblindedSignature, for: preparedMessage))
-    }
-
     func testEndToEnd() throws {
         let allNamedRFC9474Variants: [_RSA.BlindSigning.Parameters] = [
             .RSABSSA_SHA384_PSSZERO_Deterministic,
@@ -56,7 +30,29 @@ final class TestRSABlindSigningAPI: XCTestCase {
         ]
         for parameters in allNamedRFC9474Variants {
             for keySize in keySizes {
-                try testEndToEnd(parameters: parameters, keySize: keySize)
+                // [Issuer] Create key-pair (other initializers are available).
+                let privateKey = try _RSA.BlindSigning.PrivateKey(keySize: keySize, parameters: parameters)
+
+                // [Client] Create public key (other initializers are available).
+                let publicKey = privateKey.publicKey
+
+                // [Client] Have a message they wish to use.
+                let message = Data("This is some input data".utf8)
+
+                // [Client] Prepare the message.
+                let preparedMessage = publicKey.prepare(message)
+
+                // [Client] Blind the message to send to the server and get its blinding inverse.
+                let (blindedMessage, blindInverse) = try publicKey.blind(preparedMessage)
+
+                // [Issuer] Blind sign, construting the blinded message from the bytes received from the client.
+                let blindSignature = try privateKey.blindSignature(for: blindedMessage)
+
+                // [Client] Finalize using the blind inverse to unblind the signature.
+                let unblindedSignature = try publicKey.finalize(blindSignature, for: preparedMessage, blindInverse: blindInverse)
+
+                // [Verifier] Verify the unblinded signature.
+                XCTAssert(publicKey.isValidSignature(unblindedSignature, for: preparedMessage))
             }
         }
     }
