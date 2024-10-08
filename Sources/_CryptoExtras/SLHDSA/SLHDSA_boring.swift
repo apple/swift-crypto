643d07c542e33f45e7245dacb090b388cd7116df
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-10-08 14:10:34 +0200

    Add `Backing`

--- Sources/_CryptoExtras/SLHDSA/SLHDSA_boring.swift
+++ Sources/_CryptoExtras/SLHDSA/SLHDSA_boring.swift
@@ -25,170 +25,228 @@ public enum SLHDSA {}
 extension SLHDSA {
     /// A SLH-DSA-SHA2-128s private key.
     public struct PrivateKey: Sendable {
-        fileprivate let pointer: UnsafeMutablePointer<UInt8>
-        
+        private var backing: Backing
+
         public init() {
-            self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PrivateKey.bytesCount)
-
-            let publicKeyPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PublicKey.bytesCount)
-            defer { publicKeyPtr.deallocate() }
-
-            CCryptoBoringSSL_SLHDSA_SHA2_128S_generate_key(publicKeyPtr, self.pointer)
+            self.backing = Backing()
         }
 
         public init(derRepresentation: some DataProtocol) throws {
-            guard derRepresentation.count == SLHDSA.PrivateKey.bytesCount else {
-                throw CryptoKitError.incorrectKeySize
-            }
-
-            var keyDest: [UInt8] = Array(repeating: 0, count: SLHDSA.PrivateKey.bytesCount)
-            try keyDest.withUnsafeMutableBufferPointer { typedMemBuffer in
-                guard derRepresentation.copyBytes(to: typedMemBuffer) == SLHDSA.PrivateKey.bytesCount else {
-                    throw CryptoKitError.incorrectKeySize
-                }
-            }
-
-            self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PrivateKey.bytesCount)
-            self.pointer.initialize(from: keyDest, count: SLHDSA.PrivateKey.bytesCount)
+            self.backing = try Backing(derRepresentation: derRepresentation)
         }
-        
+
         public init(pemRepresentation: String) throws {
-            let document = try ASN1.PEMDocument(pemString: pemRepresentation)
-            self = try .init(derRepresentation: document.derBytes)
-        }
-        
-        public var derRepresentation: Data {
-            Data(UnsafeBufferPointer(start: self.pointer, count: SLHDSA.PrivateKey.bytesCount))
-        }
-        
-        public var pemRepresentation: String {
-            ASN1.PEMDocument(type: SLHDSA.keyType, derBytes: self.derRepresentation).pemString
+            self.backing = try Backing(pemRepresentation: pemRepresentation)
         }
 
-        fileprivate var bytes: [UInt8] {
-            return Array(UnsafeBufferPointer(start: self.pointer, count: SLHDSA.PrivateKey.bytesCount))
+        public var derRepresentation: Data {
+            self.backing.derRepresentation
+        }
+
+        public var pemRepresentation: String {
+            self.backing.pemRepresentation
         }
 
         public var publicKey: PublicKey {
-            PublicKey(privateKey: self)
+            self.backing.publicKey
         }
 
         public func signature(for data: some DataProtocol, context: [UInt8]? = nil) throws -> Signature {
-            let output = try Array<UInt8>(unsafeUninitializedCapacity: Signature.bytesCount) { bufferPtr, length in
-                let result = data.regions.first!.withUnsafeBytes { dataPtr in
-                    if let context {
-                        context.withUnsafeBytes { contextPtr in
+            try self.backing.signature(for: data, context: context)
+        }
+
+        /// The size of the private key in bytes.
+        private static let bytesCount = Backing.bytesCount
+
+        fileprivate final class Backing {
+            fileprivate let pointer: UnsafeMutablePointer<UInt8>
+            
+            init() {
+                self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PrivateKey.bytesCount)
+
+                let publicKeyPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PublicKey.bytesCount)
+                defer { publicKeyPtr.deallocate() }
+
+                CCryptoBoringSSL_SLHDSA_SHA2_128S_generate_key(publicKeyPtr, self.pointer)
+            }
+
+            init(derRepresentation: some DataProtocol) throws {
+                guard derRepresentation.count == SLHDSA.PrivateKey.Backing.bytesCount else {
+                    throw CryptoKitError.incorrectKeySize
+                }
+
+                var keyDest: [UInt8] = Array(repeating: 0, count: SLHDSA.PrivateKey.bytesCount)
+                try keyDest.withUnsafeMutableBufferPointer { typedMemBuffer in
+                    guard derRepresentation.copyBytes(to: typedMemBuffer) == SLHDSA.PrivateKey.bytesCount else {
+                        throw CryptoKitError.incorrectKeySize
+                    }
+                }
+
+                self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PrivateKey.bytesCount)
+                self.pointer.initialize(from: keyDest, count: SLHDSA.PrivateKey.bytesCount)
+            }
+            
+            convenience init(pemRepresentation: String) throws {
+                let document = try ASN1.PEMDocument(pemString: pemRepresentation)
+                try self.init(derRepresentation: document.derBytes)
+            }
+            
+            var derRepresentation: Data {
+                Data(UnsafeBufferPointer(start: self.pointer, count: SLHDSA.PrivateKey.bytesCount))
+            }
+            
+            var pemRepresentation: String {
+                ASN1.PEMDocument(type: SLHDSA.keyType, derBytes: self.derRepresentation).pemString
+            }
+
+            var publicKey: PublicKey {
+                PublicKey(privateKeyBacking: self)
+            }
+
+            func signature(for data: some DataProtocol, context: [UInt8]? = nil) throws -> Signature {
+                let output = try Array<UInt8>(unsafeUninitializedCapacity: Signature.bytesCount) { bufferPtr, length in
+                    let result = data.regions.first!.withUnsafeBytes { dataPtr in
+                        if let context {
+                            context.withUnsafeBytes { contextPtr in
+                                CCryptoBoringSSL_SLHDSA_SHA2_128S_sign(
+                                    bufferPtr.baseAddress,
+                                    self.pointer,
+                                    dataPtr.baseAddress,
+                                    dataPtr.count,
+                                    contextPtr.baseAddress,
+                                    contextPtr.count
+                                )
+                            }
+                        } else {
                             CCryptoBoringSSL_SLHDSA_SHA2_128S_sign(
                                 bufferPtr.baseAddress,
                                 self.pointer,
                                 dataPtr.baseAddress,
                                 dataPtr.count,
-                                contextPtr.baseAddress,
-                                contextPtr.count
+                                nil,
+                                0
                             )
                         }
-                    } else {
-                        CCryptoBoringSSL_SLHDSA_SHA2_128S_sign(
-                            bufferPtr.baseAddress,
-                            self.pointer,
-                            dataPtr.baseAddress,
-                            dataPtr.count,
-                            nil,
-                            0
-                        )
                     }
-                }
 
-                guard result == 1 else {
-                    throw CryptoKitError.internalBoringSSLError()
-                }
+                    guard result == 1 else {
+                        throw CryptoKitError.internalBoringSSLError()
+                    }
 
-                length = Signature.bytesCount
+                    length = Signature.bytesCount
+                }
+                return Signature(signatureBytes: output)
             }
-            return Signature(signatureBytes: output)
+            
+            /// The size of the private key in bytes.
+            static let bytesCount = 64
         }
-        
-        /// The size of the private key in bytes.
-        private static let bytesCount = 64
     }
 }
 
 extension SLHDSA {
     /// A SLH-DSA-SHA2-128s public key.
     public struct PublicKey: Sendable {
-        private let pointer: UnsafeMutablePointer<UInt8>
-        
-        fileprivate init(privateKey: PrivateKey) {
-            self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PublicKey.bytesCount)
-            CCryptoBoringSSL_SLHDSA_SHA2_128S_public_from_private(self.pointer, privateKey.pointer)
-        }
-        
-        public init(derRepresentation: some DataProtocol) throws {
-            guard derRepresentation.count == SLHDSA.PublicKey.bytesCount else {
-                throw CryptoKitError.incorrectKeySize
-            }
+        private var backing: Backing
 
-            var keyDest: [UInt8] = Array(repeating: 0, count: SLHDSA.PublicKey.bytesCount)
-            try keyDest.withUnsafeMutableBufferPointer { typedMemBuffer in
-                guard derRepresentation.copyBytes(to: typedMemBuffer) == SLHDSA.PublicKey.bytesCount else {
+        fileprivate init(privateKeyBacking: PrivateKey.Backing) {
+            self.backing = Backing(privateKeyBacking: privateKeyBacking)
+        }
+
+        public init(derRepresentation: some DataProtocol) throws {
+            self.backing = try Backing(derRepresentation: derRepresentation)
+        }
+
+        public init(pemRepresentation: String) throws {
+            self.backing = try Backing(pemRepresentation: pemRepresentation)
+        }
+
+        public var derRepresentation: Data {
+            self.backing.derRepresentation
+        }
+
+        public var pemRepresentation: String {
+            self.backing.pemRepresentation
+        }
+
+        public func isValidSignature(_ signature: Signature, for data: some DataProtocol, context: [UInt8]? = nil) -> Bool {
+            self.backing.isValidSignature(signature, for: data, context: context)
+        }
+
+        /// The size of the public key in bytes.
+        static let bytesCount = Backing.bytesCount
+
+        fileprivate final class Backing {
+            private let pointer: UnsafeMutablePointer<UInt8>
+            
+            init(privateKeyBacking: PrivateKey.Backing) {
+                self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PublicKey.bytesCount)
+                CCryptoBoringSSL_SLHDSA_SHA2_128S_public_from_private(self.pointer, privateKeyBacking.pointer)
+            }
+            
+            init(derRepresentation: some DataProtocol) throws {
+                guard derRepresentation.count == Self.bytesCount else {
                     throw CryptoKitError.incorrectKeySize
                 }
+
+                var keyDest: [UInt8] = Array(repeating: 0, count: SLHDSA.PublicKey.bytesCount)
+                try keyDest.withUnsafeMutableBufferPointer { typedMemBuffer in
+                    guard derRepresentation.copyBytes(to: typedMemBuffer) == SLHDSA.PublicKey.bytesCount else {
+                        throw CryptoKitError.incorrectKeySize
+                    }
+                }
+
+                self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PublicKey.bytesCount)
+                self.pointer.initialize(from: keyDest, count: SLHDSA.PublicKey.bytesCount)
             }
-
-            self.pointer = UnsafeMutablePointer<UInt8>.allocate(capacity: SLHDSA.PublicKey.bytesCount)
-            self.pointer.initialize(from: keyDest, count: SLHDSA.PublicKey.bytesCount)
-        }
-        
-        public init(pemRepresentation: String) throws {
-            let document = try ASN1.PEMDocument(pemString: pemRepresentation)
-            self = try .init(derRepresentation: document.derBytes)
-        }
-
-        private var bytes: [UInt8] {
-            return Array(UnsafeBufferPointer(start: self.pointer, count: SLHDSA.PublicKey.bytesCount))
-        }
-        
-        public var derRepresentation: Data {
-            return Data(UnsafeBufferPointer(start: self.pointer, count: SLHDSA.PublicKey.bytesCount))
-        }
-        
-        public var pemRepresentation: String {
-            return ASN1.PEMDocument(type: SLHDSA.publicKeyType, derBytes: self.derRepresentation).pemString
-        }
-        
-        public func isValidSignature(_ signature: Signature, for data: some DataProtocol, context: [UInt8]? = nil) -> Bool {
-            signature.withUnsafeBytes { signaturePtr in
-                let rc: CInt = data.regions.first!.withUnsafeBytes { dataPtr in
-                    if let context {
-                        context.withUnsafeBytes { contextPtr in
+            
+            convenience init(pemRepresentation: String) throws {
+                let document = try ASN1.PEMDocument(pemString: pemRepresentation)
+                try self.init(derRepresentation: document.derBytes)
+            }
+            
+            var derRepresentation: Data {
+                return Data(UnsafeBufferPointer(start: self.pointer, count: SLHDSA.PublicKey.bytesCount))
+            }
+            
+            var pemRepresentation: String {
+                return ASN1.PEMDocument(type: SLHDSA.publicKeyType, derBytes: self.derRepresentation).pemString
+            }
+            
+            func isValidSignature(_ signature: Signature, for data: some DataProtocol, context: [UInt8]? = nil) -> Bool {
+                signature.withUnsafeBytes { signaturePtr in
+                    let rc: CInt = data.regions.first!.withUnsafeBytes { dataPtr in
+                        if let context {
+                            context.withUnsafeBytes { contextPtr in
+                                CCryptoBoringSSL_SLHDSA_SHA2_128S_verify(
+                                    signaturePtr.baseAddress,
+                                    signaturePtr.count,
+                                    self.pointer,
+                                    dataPtr.baseAddress,
+                                    dataPtr.count,
+                                    contextPtr.baseAddress,
+                                    contextPtr.count
+                                )
+                            }
+                        } else {
                             CCryptoBoringSSL_SLHDSA_SHA2_128S_verify(
                                 signaturePtr.baseAddress,
                                 signaturePtr.count,
                                 self.pointer,
                                 dataPtr.baseAddress,
                                 dataPtr.count,
-                                contextPtr.baseAddress,
-                                contextPtr.count
+                                nil,
+                                0
                             )
                         }
-                    } else {
-                        CCryptoBoringSSL_SLHDSA_SHA2_128S_verify(
-                            signaturePtr.baseAddress,
-                            signaturePtr.count,
-                            self.pointer,
-                            dataPtr.baseAddress,
-                            dataPtr.count,
-                            nil,
-                            0
-                        )
                     }
+                    return rc == 1
                 }
-                return rc == 1
             }
+            
+            /// The size of the public key in bytes.
+            static let bytesCount = 32
         }
-        
-        /// The size of the public key in bytes.
-        static let bytesCount = 32
     }
 }
 
@@ -208,7 +266,7 @@ extension SLHDSA {
         /// Initialize a SLH-DSA-SHA2-128s signature from a raw representation.
         /// 
         /// - Parameter signatureBytes: The signature bytes.
-        internal init(signatureBytes: [UInt8]) {
+        init(signatureBytes: [UInt8]) {
             self.rawRepresentation = Data(signatureBytes)
         }
         
