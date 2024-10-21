acd486a4434bc45618287069d72c9d4bfec0d42c
Francesco Paolo Severino <francescopaolo.severino@gmail.com>
2024-10-21 18:33:58 +0200

    Add license header to `Optional+withUnsafeBytes.swift`

--- Sources/_CryptoExtras/Util/Optional+withUnsafeBytes.swift
+++ Sources/_CryptoExtras/Util/Optional+withUnsafeBytes.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCrypto open source project
+//
+// Copyright (c) 2024 Apple Inc. and the SwiftCrypto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.md for the list of SwiftCrypto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import Foundation
 
 extension Optional where Wrapped == ContiguousBytes {
