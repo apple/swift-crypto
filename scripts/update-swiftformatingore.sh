204c41cb7bb2aac4a135c459dd419797a0b991e9
Si Beaumont <beaumont@apple.com>
2024-11-26 16:55:12 +0000

    Add license header to update-swiftformatignore.sh

--- scripts/update-swiftformatingore.sh
+++ scripts/update-swiftformatingore.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftCrypto open source project
+##
+## Copyright (c) 2024 Apple Inc. and the SwiftCrypto project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftCrypto project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
 set -euo pipefail
 
 # The upstream soundness check only supports _excluding_ specific files using
