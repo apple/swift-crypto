3ef65592d6d7c35b6c5f43c5afcf2dbbcd98934c
Francesco Paolo Severino <96546612+fpseverino@users.noreply.github.com>
2025-06-04 16:05:58 +0200

    Add ML-DSA-87 (#357)
    Add ML-DSA-87
    
    ### Checklist
    - [X] I've run tests to see all new and existing tests pass
    - [X] I've followed the code style of the rest of the project
    - [X] I've read the [Contribution Guidelines](CONTRIBUTING.md)
    - [X] I've updated the documentation if necessary
    
    #### If you've made changes to `gyb` files
    - [X] I've run `.script/generate_boilerplate_files_with_gyb` and
    included updated generated files in a commit of this pull request
    
    ### Motivation:
    
    See #355 
    
    ### Modifications:
    
    Add the "87" parameter set in MLDSA_boring.swift.gyb, add tests for
    `MLDSA87` and update the test vectors for both ML-DSA 65 and 87
    
    ### Result:
    
    `MLDSA87` is now available

--- Tests/_CryptoExtrasTests/MLDSATests.swift
+++ Tests/_CryptoExtrasTests/MLDSATests.swift
@@ -16,7 +16,7 @@ import XCTest
 
 @testable import _CryptoExtras
 
-final class MLDSA65Tests: XCTestCase {
+final class MLDSATests: XCTestCase {
     func testMLDSA65Signing() throws {
         try testMLDSA65Signing(MLDSA65.PrivateKey())
         let seed: [UInt8] = (0..<32).map { _ in UInt8.random(in: 0...255) }
@@ -42,7 +42,32 @@ final class MLDSA65Tests: XCTestCase {
         )
     }
 
-    func testSeedRoundTripping() throws {
+    func testMLDSA87Signing() throws {
+        try testMLDSA87Signing(MLDSA87.PrivateKey())
+        let seed: [UInt8] = (0..<32).map { _ in UInt8.random(in: 0...255) }
+        try testMLDSA87Signing(MLDSA87.PrivateKey(seedRepresentation: seed))
+    }
+
+    private func testMLDSA87Signing(_ key: MLDSA87.PrivateKey) throws {
+        let test = "Hello, world!".data(using: .utf8)!
+        try XCTAssertTrue(
+            key.publicKey.isValidSignature(
+                key.signature(for: test),
+                for: test
+            )
+        )
+
+        let context = "ctx".data(using: .utf8)!
+        try XCTAssertTrue(
+            key.publicKey.isValidSignature(
+                key.signature(for: test, context: context),
+                for: test,
+                context: context
+            )
+        )
+    }
+
+    func testMLDSA65SeedRoundTripping() throws {
         let key = try MLDSA65.PrivateKey()
         let seed = key.seedRepresentation
         let roundTripped = try MLDSA65.PrivateKey(seedRepresentation: seed)
@@ -50,7 +75,15 @@ final class MLDSA65Tests: XCTestCase {
         XCTAssertEqual(key.publicKey.rawRepresentation, roundTripped.publicKey.rawRepresentation)
     }
 
-    func testSignatureIsRandomized() throws {
+    func testMLDSA87SeedRoundTripping() throws {
+        let key = try MLDSA87.PrivateKey()
+        let seed = key.seedRepresentation
+        let roundTripped = try MLDSA87.PrivateKey(seedRepresentation: seed)
+        XCTAssertEqual(seed, roundTripped.seedRepresentation)
+        XCTAssertEqual(key.publicKey.rawRepresentation, roundTripped.publicKey.rawRepresentation)
+    }
+
+    func testMLDSA65SignatureIsRandomized() throws {
         let message = "Hello, world!".data(using: .utf8)!
 
         let seed: [UInt8] = (0..<32).map { _ in UInt8.random(in: 0...255) }
@@ -67,7 +100,24 @@ final class MLDSA65Tests: XCTestCase {
         XCTAssertTrue(publicKey.isValidSignature(signature2, for: message))
     }
 
-    func testInvalidPublicKeyEncodingLength() throws {
+    func testMLDSA87SignatureIsRandomized() throws {
+        let message = "Hello, world!".data(using: .utf8)!
+
+        let seed: [UInt8] = (0..<32).map { _ in UInt8.random(in: 0...255) }
+        let key = try MLDSA87.PrivateKey(seedRepresentation: seed)
+        let publicKey = key.publicKey
+
+        let signature1 = try key.signature(for: message)
+        let signature2 = try key.signature(for: message)
+
+        XCTAssertNotEqual(signature1, signature2)
+
+        // Even though the signatures are different, they both verify.
+        XCTAssertTrue(publicKey.isValidSignature(signature1, for: message))
+        XCTAssertTrue(publicKey.isValidSignature(signature2, for: message))
+    }
+
+    func testInvalidMLDSA65PublicKeyEncodingLength() throws {
         // Encode a public key with a trailing 0 at the end.
         var encodedPublicKey = [UInt8](repeating: 0, count: MLDSA65.PublicKey.byteCount + 1)
         let seed: [UInt8] = (0..<32).map { _ in UInt8.random(in: 0...255) }
@@ -87,12 +137,42 @@ final class MLDSA65Tests: XCTestCase {
         XCTAssertThrowsError(try MLDSA65.PublicKey(rawRepresentation: encodedPublicKey))
     }
 
+    func testInvalidMLDSA87PublicKeyEncodingLength() throws {
+        // Encode a public key with a trailing 0 at the end.
+        var encodedPublicKey = [UInt8](repeating: 0, count: MLDSA87.PublicKey.byteCount + 1)
+        let seed: [UInt8] = (0..<32).map { _ in UInt8.random(in: 0...255) }
+        let key = try MLDSA87.PrivateKey(seedRepresentation: seed)
+        let publicKey = key.publicKey
+        encodedPublicKey.replaceSubrange(0..<MLDSA87.PublicKey.byteCount, with: publicKey.rawRepresentation)
+
+        // Public key is 1 byte too short.
+        let shortPublicKey = Array(encodedPublicKey.prefix(MLDSA87.PublicKey.byteCount - 1))
+        XCTAssertThrowsError(try MLDSA87.PublicKey(rawRepresentation: shortPublicKey))
+
+        // Public key has the correct length.
+        let correctLengthPublicKey = Array(encodedPublicKey.prefix(MLDSA87.PublicKey.byteCount))
+        XCTAssertNoThrow(try MLDSA87.PublicKey(rawRepresentation: correctLengthPublicKey))
+
+        // Public key is 1 byte too long.
+        XCTAssertThrowsError(try MLDSA87.PublicKey(rawRepresentation: encodedPublicKey))
+    }
+
     func testMLDSA65NISTKeyGenFile() throws {
-        try nistTest(jsonName: "mldsa_65_nist_keygen_tests") { (testVector: NISTKeyGenTestVector) in
+        try nistTest(jsonName: "mldsa_nist_keygen_65_tests") { (testVector: NISTKeyGenTestVector) in
             let seed = try Data(hexString: testVector.seed)
             let publicKey = try MLDSA65.PublicKey(rawRepresentation: Data(hexString: testVector.pub))
 
             let expectedkey = try MLDSA65.PrivateKey(seedRepresentation: seed).publicKey
+            XCTAssertEqual(publicKey.rawRepresentation, expectedkey.rawRepresentation)
+        }
+    }
+
+    func testMLDSA87NISTKeyGenFile() throws {
+        try nistTest(jsonName: "mldsa_nist_keygen_87_tests") { (testVector: NISTKeyGenTestVector) in
+            let seed = try Data(hexString: testVector.seed)
+            let publicKey = try MLDSA87.PublicKey(rawRepresentation: Data(hexString: testVector.pub))
+
+            let expectedkey = try MLDSA87.PrivateKey(seedRepresentation: seed).publicKey
             XCTAssertEqual(publicKey.rawRepresentation, expectedkey.rawRepresentation)
         }
     }
@@ -117,18 +197,8 @@ final class MLDSA65Tests: XCTestCase {
         for _ in 0..<2 {
             fileURL.deleteLastPathComponent()
         }
-        #if compiler(>=6.0)
-        if #available(macOS 13, iOS 16, watchOS 9, tvOS 16, visionOS 1, macCatalyst 16, *) {
-            fileURL.append(path: "_CryptoExtrasVectors", directoryHint: .isDirectory)
-            fileURL.append(path: "\(jsonName).json", directoryHint: .notDirectory)
-        } else {
-            fileURL = fileURL.appendingPathComponent("_CryptoExtrasVectors", isDirectory: true)
-            fileURL = fileURL.appendingPathComponent("\(jsonName).json", isDirectory: false)
-        }
-        #else
         fileURL = fileURL.appendingPathComponent("_CryptoExtrasVectors", isDirectory: true)
         fileURL = fileURL.appendingPathComponent("\(jsonName).json", isDirectory: false)
-        #endif
 
         let data = try Data(contentsOf: fileURL)
 
@@ -140,10 +210,42 @@ final class MLDSA65Tests: XCTestCase {
     }
 
     func testMLDSA65WycheproofVerifyFile() throws {
-        try wycheproofTest(jsonName: "mldsa_65_standard_verify_test") { (testGroup: WycheproofTestGroup) in
+        try wycheproofTest(jsonName: "mldsa_65_verify_test") { (testGroup: WycheproofTestGroup) in
             let publicKey: MLDSA65.PublicKey
             do {
                 publicKey = try MLDSA65.PublicKey(rawRepresentation: Data(hexString: testGroup.publicKey))
+            } catch {
+                if testGroup.tests.contains(where: { $0.flags.contains(.incorrectPublicKeyLength) }) { return }
+                throw error
+            }
+            for test in testGroup.tests {
+                let message = try Data(hexString: test.msg)
+                let signature = try Data(hexString: test.sig)
+                let context = try test.ctx.map { try Data(hexString: $0) }
+
+                switch test.result {
+                case .valid:
+                    if let context {
+                        XCTAssertTrue(publicKey.isValidSignature(signature, for: message, context: context))
+                    } else {
+                        XCTAssertTrue(publicKey.isValidSignature(signature, for: message))
+                    }
+                case .invalid:
+                    if let context {
+                        XCTAssertFalse(publicKey.isValidSignature(signature, for: message, context: context))
+                    } else {
+                        XCTAssertFalse(publicKey.isValidSignature(signature, for: message))
+                    }
+                }
+            }
+        }
+    }
+
+    func testMLDSA87WycheproofVerifyFile() throws {
+        try wycheproofTest(jsonName: "mldsa_87_verify_test") { (testGroup: WycheproofTestGroup) in
+            let publicKey: MLDSA87.PublicKey
+            do {
+                publicKey = try MLDSA87.PublicKey(rawRepresentation: Data(hexString: testGroup.publicKey))
             } catch {
                 if testGroup.tests.contains(where: { $0.flags.contains(.incorrectPublicKeyLength) }) { return }
                 throw error
