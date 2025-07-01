f4e935a34780160bd5828acb979fc9b9732699d7
Raphael Hiesgen <r_hiesgen@apple.com>
2025-07-01 10:08:51 +0100

    Object identifiers should not be public here

--- Sources/_CryptoExtras/EC/ObjectIdentifier.swift
+++ Sources/_CryptoExtras/EC/ObjectIdentifier.swift
@@ -19,20 +19,20 @@ extension ASN1ObjectIdentifier.AlgorithmIdentifier {
     // Identifies the key agreement algorithm X25519.
     //
     // This identifier is defined in RFC 8410
-    public static let idX25519: ASN1ObjectIdentifier = [1, 3, 101, 110]
+    static let idX25519: ASN1ObjectIdentifier = [1, 3, 101, 110]
 
     // Identifies the key agreement algorithm X448.
     //
     // This identifier is defined in RFC 8410
-    public static let idX448: ASN1ObjectIdentifier = [1, 3, 101, 111]
+    static let idX448: ASN1ObjectIdentifier = [1, 3, 101, 111]
 
     // Identifies the signature algorithm Ed25519.
     //
     // This identifier is defined in RFC 8410
-    public static let idEd25519: ASN1ObjectIdentifier = [1, 3, 101, 112]
+    static let idEd25519: ASN1ObjectIdentifier = [1, 3, 101, 112]
 
     // Identifies the signature algorithm Ed448.
     //
     // This identifier is defined in RFC 8410
-    public static let idEd448: ASN1ObjectIdentifier = [1, 3, 101, 113]
+    static let idEd448: ASN1ObjectIdentifier = [1, 3, 101, 113]
 }
