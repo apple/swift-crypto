5bf6045e5a25fe15272cc5c0171c64b421c72782
Cory Benfield <lukasa@apple.com>
2025-08-04 14:53:50 +0100

    License headers

--- Tests/_CryptoExtrasTests/CMACTests.swift
+++ Tests/_CryptoExtrasTests/CMACTests.swift
@@ -1,3 +1,16 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the SwiftCrypto open source project
+//
+// Copyright (c) 2025 Apple Inc. and the SwiftCrypto project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of SwiftCrypto project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
 import Crypto
 import _CryptoExtras
 import Foundation
