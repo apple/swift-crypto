b2dc7c121512543dbaac275ae6fa1181af94c8c6
Si Beaumont <beaumont@apple.com>
2025-08-08 15:43:00 +0100

    fixup: Test should guarantee threads concurrently live

--- Tests/_CryptoExtrasTests/ECToolbox/BoringSSL/ECToolboxBoringSSLTests.swift
+++ Tests/_CryptoExtrasTests/ECToolbox/BoringSSL/ECToolboxBoringSSLTests.swift
@@ -27,24 +27,33 @@ final class ECToolboxBoringSSLTests: XCTestCase {
     func testThreadLocalFFAC(_ Curve: (some OpenSSLSupportedNISTCurve & Sendable).Type) async {
         let numThreads = 3
         let numReadsPerThread = 2
-        var threads: [Thread] = []
+
+        var threads: [(thread: Thread, thisThreadDidReads: XCTestExpectation, allThreadsDidReads: XCTestExpectation, thisThreadFinished: XCTestExpectation)] = []
+
         var objectIdentifiers: [(threadID: Int, ffacID: ObjectIdentifier)] = []
         let lock = NSLock()
-        let expectation = XCTestExpectation(description: "Thread done")
-        expectation.expectedFulfillmentCount = numThreads
+
         for i in 1...numThreads {
+            let thisThreadDidReads = expectation(description: "this thread did its reads")
+            let allThreadsDidReads = expectation(description: "all threads did their reads")
+            let thisThreadFinished = expectation(description: "this thread is finished")
             let thread = Thread {
-                lock.lock()
-                defer { lock.unlock() }
                 for _ in 1...numReadsPerThread {
+                    lock.lock()
                     objectIdentifiers.append((i, ObjectIdentifier(Curve.__ffac)))
+                    lock.unlock()
                 }
-                expectation.fulfill()
+                thisThreadDidReads.fulfill()
+                XCTWaiter().wait(for: [allThreadsDidReads], timeout: .greatestFiniteMagnitude)
+                thisThreadFinished.fulfill()
             }
-            threads.append(thread)
+            thread.name = "thread-\(i)"
+            threads.append((thread, thisThreadDidReads, allThreadsDidReads, thisThreadFinished))
             thread.start()
         }
-        await fulfillment(of: [expectation], timeout: 0.5)
+        await fulfillment(of: threads.map(\.thisThreadDidReads), timeout: 0.5)
+        threads.forEach { $0.allThreadsDidReads.fulfill() }
+        await fulfillment(of: threads.map(\.thisThreadFinished), timeout: 0.5)
 
         XCTAssertEqual(objectIdentifiers.count, numThreads * numReadsPerThread)
         for threadID in 1...numThreads {
