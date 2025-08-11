13075b8a8b962e24dbdb7c6945e6e2bec4841126
Si Beaumont <beaumont@apple.com>
2025-08-11 16:19:14 +0100

    Add licence header to vendoring script

--- scripts/vendor-xkcp.sh
+++ scripts/vendor-xkcp.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftCrypto open source project
+##
+## Copyright (c) 2019-2021 Apple Inc. and the SwiftCrypto project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftCrypto project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
 set -euo pipefail
 
 log() { printf -- "** %s\n" "$*" >&2; }
