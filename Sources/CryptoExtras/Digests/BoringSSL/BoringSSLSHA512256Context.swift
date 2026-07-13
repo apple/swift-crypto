70712b39f7efc84c6fbb9a5cba4b605c99f4820c
Si Beaumont <beaumont@apple.com>
2026-07-13 16:16:14 +0100

    (split: SHA512256) Enable CheckImplementationOnly and box properties where needed
    We've long since used `@_implementationOnly import` (IOI) for our
    vendored C modules (BoringSSL and libXKCP) in an attempt to not leak our
    dependency into downstream builds. This is important because leaking
    might cause symbol collisions (build failures) or, worse, layout
    mismatches (latent runtime failures).
    
    IOI was intended to be used with library evolution, which this project
    does not support, and use of IOI without library evolution has been
    soft-deprecated. Users are encouraged to move to `internal import` (II),
    which is usually the right choice for Swift packages. But II does _not_
    provide the same guarantees as IOI and, for this project, we really need
    the protections given by IOI so we will continue to use it, even with
    library evolution disabled.
    
    One of the reasons for the soft-deprecation was the implementation of
    IOI used an incomplete checker, that won't catch all interface leaks
    without library evolution. However, Swift has also introduced a new
    feature, `CheckImplementationOnly` (CIO), which enables the stricter
    interface checking that we need.
    
    Enabling CIO for this project today actually exposes a number of places
    where we _are_ leaking the C types into our interface in ways that could
    cause build- or runtime problems in downstream projects, and rightly
    causes the build to fail. These types will need to have their C struct
    members boxed behind an `OpaquePointer`.
    
    This patch enables CIO, and adds the required boxing to the offending
    types.
    
    The public API is unchanged.
    
    Additional refactoring is also done to ensure a single designated
    initializer, responsible for the allocation, forcing all convenience
    initializers through an `init(takingOwnershipOf:)` spelling, to reduce
    the chances of introducing future double free bugs in complex
    initializers.
    
    This patch also adds some benchmarks for some of the paths that would be
    implicated by these changes. While they show the expected increase in
    allocations, there's only a modest-to-negligible performance regression,
    since the majority of the compute is dominated by the time spent doing
    the crypto routines in the C library themselves.

--- Sources/CryptoExtras/Digests/BoringSSL/BoringSSLSHA512256Context.swift
+++ Sources/CryptoExtras/Digests/BoringSSL/BoringSSLSHA512256Context.swift
@@ -21,45 +21,51 @@ import Darwin
 
 @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, macCatalyst 13, visionOS 1.0, *)
 final class BoringSSLSHA512256Context {
-    private var context: SHA512_CTX
+    // @_implementationOnly import => must use OpaquePointer for stored property.
+    private let _storage: OpaquePointer
+    private var context: UnsafeMutablePointer<SHA512_CTX> {
+        UnsafeMutablePointer(self._storage)
+    }
 
-    init() {
-        guard let context = BoringSSLSHA512256HashFunction.initialize() else {
-            preconditionFailure("Unable to initialize digest state")
-        }
-        self.context = context
+    /// This is the only designated initializer, responsible for the allocation. Deallocation happens in deinit.
+    /// To reduce mistakes (e.g. double-free), all other initializers should be convenience initializers.
+    private init(takingOwnershipOf pointer: UnsafeMutablePointer<SHA512_CTX>) {
+        self._storage = OpaquePointer(pointer)
     }
 
     deinit {
-        withUnsafeMutablePointer(to: &self.context) {
-            $0.zeroize()
+        self.context.zeroize()
+        self.context.deinitialize(count: 1)
+        self.context.deallocate()
+    }
+
+    convenience init() {
+        let ptr = UnsafeMutablePointer<SHA512_CTX>.allocate(capacity: 1)
+        ptr.initialize(to: SHA512_CTX())
+        self.init(takingOwnershipOf: ptr)
+
+        guard CCryptoBoringSSL_SHA512_256_Init(self.context) == 1 else {
+            preconditionFailure("Unable to initialize digest state")
         }
     }
 
-    init(copying original: BoringSSLSHA512256Context) {
-        self.context = original.context
+    convenience init(copying original: BoringSSLSHA512256Context) {
+        let ptr = UnsafeMutablePointer<SHA512_CTX>.allocate(capacity: 1)
+        ptr.initialize(to: original.context.pointee)
+        self.init(takingOwnershipOf: ptr)
     }
 
     func update(bufferPointer data: UnsafeRawBufferPointer) {
-        guard BoringSSLSHA512256HashFunction.update(&self.context, data: data) else {
+        guard CCryptoBoringSSL_SHA512_256_Update(self.context, data.baseAddress, data.count) == 1 else {
             preconditionFailure("Unable to update digest state")
         }
     }
 
     func finalize() -> SHA512256Digest {
-        var copyContext = self.context
-        defer {
-            withUnsafeMutablePointer(to: &copyContext) {
-                $0.zeroize()
-            }
-        }
-        return withUnsafeTemporaryAllocation(byteCount: BoringSSLSHA512256HashFunction.digestSize, alignment: 1) {
-            digestPointer in
-            defer {
-                digestPointer.zeroize()
-            }
-
-            guard BoringSSLSHA512256HashFunction.finalize(&copyContext, digest: digestPointer) else {
+        var contextCopy = self.context.pointee
+        defer { withUnsafeMutablePointer(to: &contextCopy) { $0.zeroize() } }
+        return withUnsafeTemporaryAllocation(byteCount: SHA512256Digest.byteCount, alignment: 1) { digestPointer in
+            guard CCryptoBoringSSL_SHA512_256_Final(digestPointer.baseAddress, &contextCopy) == 1 else {
                 preconditionFailure("Unable to finalize digest state")
             }
             // We force unwrap here because if the digest size is wrong it's an internal error.
